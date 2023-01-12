b0d0a02aef6f7cdae49b2b80ab72ec8c221ee6dd
Faris Abbasi <mfarisabbasi@gmail.com>
2023-01-12 16:05:02 +0500

    added tokenFunctions

---
+++ controllers/authController.js
@@ -0,0 +1,8 @@
+// Packages Import
+import asyncHandler from "express-async-handler";
+
+// Models Import
+import User from "../models/userModel.js";
+
+// Functions Import
+import { generateToken } from "../functions/tokenFunctions.js";
