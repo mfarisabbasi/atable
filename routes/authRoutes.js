78f4cd2a5624aa1afd55a21c2fcfc2c340ad6871
Faris Abbasi <mfarisabbasi@gmail.com>
2023-01-12 16:30:35 +0500

    added authRoutes

---
+++ routes/authRoutes.js
@@ -0,0 +1,13 @@
+// Package Imports
+import express from "express";
+
+// Middleware Imports
+
+// Controller Imports
+import { createNewUserWithEmail } from "../controllers/authController.js";
+
+const router = express.Router();
+
+router.post("/new/email", createNewUserWithEmail);
+
+export default router;
