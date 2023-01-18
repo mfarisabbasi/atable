661fb4f78c8c86ea1abff8e1eb66b4fc87263ab6
Faris Abbasi <mfarisabbasi@gmail.com>
2023-01-18 17:20:00 +0500

    app start log msg fixed

--- server.js
+++ server.js
@@ -62,5 +62,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server Running In ${process.env.NODE_END} On PORT ${PORT}`)
+  console.log(`Server Running In ${process.env.NODE_ENV} On PORT ${PORT}`)
 );
