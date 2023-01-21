ba75c9bf20d38cd28553010f94345869ee400d22
Faris Abbasi <farisabbasi@Fariss-MacBook-Air.local>
2023-01-21 17:38:33 +0500

    removed createResetPasswordToken method -> userModel

--- models/userModel.js
+++ models/userModel.js
@@ -43,19 +43,6 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-userSchema.methods.createPasswordResetToken = function () {
-  const resetToken = crypto.randomBytes(32).toString("hex");
-
-  this.passwordResetToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
-
-  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
-
-  return resetToken;
-};
-
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     next();
