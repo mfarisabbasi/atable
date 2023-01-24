81e8351c09ff12b6e49283b9a918dc5659d0673d
Faris Abbasi <farisabbasi@Fariss-MacBook-Air.local>
2023-01-24 13:40:04 +0500

    removed crypto import

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
-import crypto from "crypto";
 
 const userSchema = mongoose.Schema({
   provider: {
