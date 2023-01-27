d74a98cd6fdb6384d38530f238349dd43676630e
Faris Abbasi <farisabbasi@Fariss-MacBook-Air.local>
2023-01-27 17:15:00 +0500

    added adminModel

---
+++ models/adminModel.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcryptjs";
+
+const adminSchema = mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is invalid");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+adminSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+adminSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+const Admin = mongoose.model("Admin", adminSchema);
+export default Admin;
