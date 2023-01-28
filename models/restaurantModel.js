3b237367441540b6181e3b02ea29233441afce56
Faris Abbasi <farisabbasi@Fariss-MacBook-Air.local>
2023-01-28 18:42:15 +0500

    added restaurantModel

---
+++ models/restaurantModel.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+
+const restaurantSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    cuisine: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return /^(00212|\+212|0)[67]\d{8}$/.test(v);
+        },
+        message: "{VALUE} is not a valid phone number",
+      },
+    },
+    openingHours: {
+      type: Map,
+      of: String,
+    },
+    images: [{ type: String }],
+    rating: {
+      type: Number,
+      min: 0,
+      max: 5,
+    },
+    totalRatings: {
+      type: Number,
+      default: 0,
+    },
+    menu: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Menu",
+      },
+    ],
+    reservations: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Reservation",
+      },
+    ],
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "resOwner",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Restaurant = mongoose.model("Restaurant", restaurantSchema);
+export default Restaurant;
