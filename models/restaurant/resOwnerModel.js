a4b3ce62e766a79e0468f20a879d12b46a57c59a
Faris Abbasi <farisabbasi@Fariss-MacBook-Air.local>
2023-01-30 19:08:28 +0500

    added restaurantName field

--- models/restaurant/resOwnerModel.js
+++ models/restaurant/resOwnerModel.js
@@ -8,6 +8,10 @@ const resOwnerSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    restaurantName: {
+      type: String,
+      required: true,
+    },
     email: {
       type: String,
       required: true,
