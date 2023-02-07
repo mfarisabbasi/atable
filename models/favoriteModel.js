d52555cd3b93ffdddad53cb400170b38295d639c
Faris Abbasi <farisabbasi@Fariss-MacBook-Air.local>
2023-02-07 14:48:53 +0500

    added favoriteModel

---
+++ models/favoriteModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const favoriteSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    favorites: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Restaurant",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Favorite = mongoose.model("Favorite", favoriteSchema);
+export default Favorite;
