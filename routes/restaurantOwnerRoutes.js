520073b761d1a1d1e60ece4585ff1a61b2bb4284
anas <toanasbinimran@gmail.com>
2023-03-01 16:15:50 +0500

    added all the routes to of restaurantOwner

--- routes/restaurantOwnerRoutes.js
+++ routes/restaurantOwnerRoutes.js
@@ -2,6 +2,7 @@
 import express from "express";
 // Middleware Imports
 import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { checkResOwner } from "../middlewares/checkRestaurantAdmin.js";
 // Controller Imports
 import {
   authResOwner,
@@ -16,5 +17,16 @@ router.post("/owner/auth", authResOwner);
 router.post("/owner/menu/new", authMiddleware, createNewMenu);
 router.post("/owner/menu/item/new", authMiddleware, createNewMenuItem);
 router.get("/test", authMiddleware, testResOwner);
+router.get("/owner/show/todayreservations",checkResOwner,getTodayBookings);
+router.get("/owner/show/monthlyreservations",checkResOwner,checkMonthlyReservation);
+router.get("/owner/show/allreservations",checkResOwner,disaplyReservationsRequest);
+router.post("/owner/reservation/approve",checkResOwner,approveReservationById);
+router.post("/owner/reservation/cancel",checkResOwner,cancelReservationById);
+router.post("/owner/openslots/add",checkResOwner,addRestaurantOpeningSlots);
+router.post("/owner/closeslots/add",checkResOwner,addRestaurantClosingSlots);
+router.post("/owner/reservation/toggle/auto",checkResOwner,toggleAutoApprove);
+router.post("/owner/reservation/auto",checkResOwner,autoApproveReservations);
+router.post("/owner/reservation/add/tables",checkResOwner,addTables)
+router.get("/owner/show/profile",checkResOwner,restaurantProfile)
 
 export default router;
